20a7b45c22df76ed63afbbe500f634e0ea57c3a2
snake <lcpdfk@163.com>
2021-03-03 19:10:39 +0800

    704. 二分查找

--- Binary_Search/mian.cpp
+++ Binary_Search/mian.cpp
@@ -1,9 +1,12 @@
-﻿// Binary_Search.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include "solution.h"
+using namespace std;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+//704. 二分查找
+int main(){
+    Solution solution;
+    vector<int> nums = { -1,0,3,5,9,12 };
+    cout << solution.search(nums, 9) << endl;
+    system("pause");
+    return 0;
 }
---
+++ Binary_Search/solution.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int search(vector<int>& nums, int target);
+};
+
